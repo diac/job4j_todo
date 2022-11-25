6551676f97b6edff283c1a672ce62cc7614bc0ed
diac <nikolai.gladkikh@gmail.com>
2022-11-25 13:47:42 +0300

    Добавить инициализацию Spring Boot в Main

--- src/main/java/ru/job4j/todo/Main.java
+++ src/main/java/ru/job4j/todo/Main.java
@@ -4,8 +4,11 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@SpringBootApplication
 public class Main {
 
     @Bean(destroyMethod = "close")
@@ -16,6 +19,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Job4J TODO");
+        SpringApplication.run(Main.class, args);
     }
 }
