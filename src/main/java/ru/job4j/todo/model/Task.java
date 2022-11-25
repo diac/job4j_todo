022d886e3553a39391ad09a54c146959c9b55940
diac <nikolai.gladkikh@gmail.com>
2022-11-25 13:59:47 +0300

    Добавить модель Task

---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,24 @@
+package ru.job4j.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String description;
+    private LocalDateTime created;
+    private boolean done;
+}
