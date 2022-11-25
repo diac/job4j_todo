28658b2cb7f390647842600cb9a350c4fcde443a
diac <nikolai.gladkikh@gmail.com>
2022-11-25 17:49:58 +0300

    Добавить страницу просмотра задачи по ID

--- src/main/java/ru/job4j/todo/controller/TaskController.java
+++ src/main/java/ru/job4j/todo/controller/TaskController.java
@@ -4,15 +4,16 @@ import lombok.AllArgsConstructor;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.job4j.todo.model.Task;
 import ru.job4j.todo.service.TaskService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @ThreadSafe
@@ -52,5 +53,16 @@ public class TaskController {
     public String store(@ModelAttribute Task task) {
         taskService.add(task);
         return "redirect:/";
+    }
+
+    @GetMapping("/tasks/{id}")
+    public String view(@PathVariable("id") int id, Model model, RedirectAttributes redirectAttributes) {
+        Optional<Task> task = taskService.findById(id);
+        if (task.isEmpty()) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Задача не найдена");
+            return "redirect:/";
+        }
+        model.addAttribute("task", task.get());
+        return "tasks/view";
     }
 }
