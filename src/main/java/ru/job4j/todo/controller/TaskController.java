b50ba0382d0c73cb0fe1b74f35bf130fe816a6dc
diac <nikolai.gladkikh@gmail.com>
2022-11-25 17:07:05 +0300

    Добавить страницу со всеми задачами

--- src/main/java/ru/job4j/todo/controller/TaskController.java
+++ src/main/java/ru/job4j/todo/controller/TaskController.java
@@ -3,8 +3,12 @@ package ru.job4j.todo.controller;
 import lombok.AllArgsConstructor;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.todo.model.Task;
 import ru.job4j.todo.service.TaskService;
+
+import java.util.List;
 
 @Controller
 @ThreadSafe
@@ -12,4 +16,11 @@ import ru.job4j.todo.service.TaskService;
 public class TaskController {
 
     private final TaskService taskService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Task> tasks = taskService.findAll();
+        model.addAttribute("tasks", tasks);
+        return "tasks/index";
+    }
 }
