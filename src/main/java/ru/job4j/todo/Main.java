62bf6975f337c0a837138e7ca5775fa4f376faef
diac <nikolai.gladkikh@gmail.com>
2022-11-25 12:03:25 +0300

    Добавить бин SessionFactory в классе Main

--- src/main/java/ru/job4j/todo/Main.java
+++ src/main/java/ru/job4j/todo/Main.java
@@ -1,6 +1,19 @@
 package ru.job4j.todo;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.context.annotation.Bean;
+
 public class Main {
+
+    @Bean(destroyMethod = "close")
+    public SessionFactory sf() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
 
     public static void main(String[] args) {
         System.out.println("Job4J TODO");
