87cd1e45e239d0c8ae11d55f33f0b24d654ac2f5
diac <nikolai.gladkikh@gmail.com>
2022-11-30 11:13:46 +0300

    Исправить ошибки в тесте HibernateTaskRepositoryTest

--- src/test/java/ru/job4j/todo/repository/HibernateTaskRepositoryTest.java
+++ src/test/java/ru/job4j/todo/repository/HibernateTaskRepositoryTest.java
@@ -5,39 +5,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.job4j.todo.config.DataSourceConfig;
 import ru.job4j.todo.model.Task;
+import ru.job4j.todo.model.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = {DataSourceConfig.class, HibernateCrudRepository.class, HibernateTaskRepository.class})
+@SpringBootTest(classes = {
+        DataSourceConfig.class,
+        HibernateCrudRepository.class,
+        HibernateTaskRepository.class,
+        HibernateUserRepository.class
+})
 public class HibernateTaskRepositoryTest {
 
     @Autowired
-    private TaskRepository repository;
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     public void whenCreateTask() {
-        String taskDescription = String.valueOf(System.currentTimeMillis());
-        Task task = new Task(0, taskDescription, LocalDateTime.now(), false);
-        repository.add(task);
-        Task taskInDb = repository.findById(task.getId())
-                .orElse(new Task(0, null, LocalDateTime.now(), false));
+        String value = String.valueOf(System.currentTimeMillis());
+        User user = new User(0, value, value, value);
+        userRepository.add(user);
+        Task task = new Task(0, value, LocalDateTime.now(), false, user);
+        taskRepository.add(task);
+        Task taskInDb = taskRepository.findById(task.getId())
+                .orElse(new Task(0, null, LocalDateTime.now(), false, null));
         assertThat(taskInDb.getId()).isEqualTo(task.getId());
         assertThat(taskInDb.getDescription()).isEqualTo(task.getDescription());
     }
 
     @Test
     public void whenUpdateTask() {
-        String taskDescription = String.valueOf(System.currentTimeMillis());
-        Task task = new Task(0, taskDescription, LocalDateTime.now(), false);
-        repository.add(task);
+        String value = String.valueOf(System.currentTimeMillis());
+        User user = new User(0, value, value, value);
+        userRepository.add(user);
+        Task task = new Task(0, value, LocalDateTime.now(), false, user);
+        taskRepository.add(task);
         task.setDescription(task.getDescription() + "_updated");
         task.setDone(true);
-        repository.update(task);
-        Task taskInDb = repository.findById(task.getId())
-                .orElse(new Task(0, null, LocalDateTime.now(), false));
+        taskRepository.update(task);
+        Task taskInDb = taskRepository.findById(task.getId())
+                .orElse(new Task(0, null, LocalDateTime.now(), false, null));
         assertThat(taskInDb.getId()).isEqualTo(task.getId());
         assertThat(taskInDb.getDescription()).isEqualTo(task.getDescription());
         assertThat(taskInDb.isDone()).isEqualTo(task.isDone());
@@ -45,75 +58,93 @@ public class HibernateTaskRepositoryTest {
 
     @Test
     public void whenDeleteTask() {
-        String taskDescription = String.valueOf(System.currentTimeMillis());
-        Task task = new Task(0, taskDescription, LocalDateTime.now(), false);
-        repository.add(task);
+        String value = String.valueOf(System.currentTimeMillis());
+        User user = new User(0, value, value, value);
+        userRepository.add(user);
+        Task task = new Task(0, value, LocalDateTime.now(), false, user);
+        taskRepository.add(task);
         int taskId = task.getId();
-        repository.delete(task);
-        assertThat(repository.findById(taskId)).isEmpty();
+        taskRepository.delete(task);
+        assertThat(taskRepository.findById(taskId)).isEmpty();
     }
 
     @Test
     public void whenDeleteById() {
-        String taskDescription = String.valueOf(System.currentTimeMillis());
-        Task task = new Task(0, taskDescription, LocalDateTime.now(), false);
-        repository.add(task);
+        String value = String.valueOf(System.currentTimeMillis());
+        User user = new User(0, value, value, value);
+        userRepository.add(user);
+        Task task = new Task(0, value, LocalDateTime.now(), false, user);
+        taskRepository.add(task);
         int taskId = task.getId();
-        repository.deleteById(task.getId());
-        assertThat(repository.findById(taskId)).isEmpty();
+        taskRepository.deleteById(task.getId());
+        assertThat(taskRepository.findById(taskId)).isEmpty();
     }
 
     @Test
     public void whenFindAllByDone() {
-        String taskDescription = String.valueOf(System.currentTimeMillis());
-        Task task = new Task(0, taskDescription, LocalDateTime.now(), true);
-        repository.add(task);
-        List<Task> tasks = repository.findAllByDone(true);
+        String value = String.valueOf(System.currentTimeMillis());
+        User user = new User(0, value, value, value);
+        userRepository.add(user);
+        Task task = new Task(0, value, LocalDateTime.now(), true, user);
+        taskRepository.add(task);
+        List<Task> tasks = taskRepository.findAllByDone(true);
         assertThat(tasks.contains(task)).isTrue();
     }
 
     @Test
     public void whenSetDescriptionById() {
-        Task task = new Task(0, "test", LocalDateTime.now(), true);
-        repository.add(task);
-        repository.setDescriptionById(task.getId(), task.getDescription() + "_updated");
-        Task taskInDb = repository.findById(task.getId())
-                .orElse(new Task(0, null, LocalDateTime.now(), false));
+        String value = String.valueOf(System.currentTimeMillis());
+        User user = new User(0, value, value, value);
+        userRepository.add(user);
+        Task task = new Task(0, "test", LocalDateTime.now(), true, user);
+        taskRepository.add(task);
+        taskRepository.setDescriptionById(task.getId(), task.getDescription() + "_updated");
+        Task taskInDb = taskRepository.findById(task.getId())
+                .orElse(new Task(0, null, LocalDateTime.now(), false, null));
         assertThat(task).isEqualTo(taskInDb);
         assertThat("test_updated").isEqualTo(taskInDb.getDescription());
     }
 
     @Test
     public void whenSetDescription() {
-        Task task = new Task(0, "test", LocalDateTime.now(), true);
-        repository.add(task);
-        repository.setDescription(task, task.getDescription() + "_updated");
-        Task taskInDb = repository.findById(task.getId())
-                .orElse(new Task(0, null, LocalDateTime.now(), false));
+        String value = String.valueOf(System.currentTimeMillis());
+        User user = new User(0, value, value, value);
+        userRepository.add(user);
+        Task task = new Task(0, "test", LocalDateTime.now(), true, user);
+        taskRepository.add(task);
+        taskRepository.setDescription(task, task.getDescription() + "_updated");
+        Task taskInDb = taskRepository.findById(task.getId())
+                .orElse(new Task(0, null, LocalDateTime.now(), false, null));
         assertThat(task).isEqualTo(taskInDb);
         assertThat("test_updated").isEqualTo(taskInDb.getDescription());
     }
 
     @Test
     public void whenSetDone() {
-        Task task = new Task(0, null, LocalDateTime.now(), true);
-        repository.add(task);
+        String value = String.valueOf(System.currentTimeMillis());
+        User user = new User(0, value, value, value);
+        userRepository.add(user);
+        Task task = new Task(0, null, LocalDateTime.now(), true, user);
+        taskRepository.add(task);
         boolean done = task.isDone();
-        repository.setDone(task, false);
-        Task taskInDb = repository.findById(task.getId())
-                .orElse(new Task(0, null, LocalDateTime.now(), false));
+        taskRepository.setDone(task, false);
+        Task taskInDb = taskRepository.findById(task.getId())
+                .orElse(new Task(0, null, LocalDateTime.now(), false, null));
         assertThat(task).isEqualTo(taskInDb);
         assertThat(done).isNotEqualTo(taskInDb.isDone());
     }
 
     @Test
     public void whenSetDoneById() {
-        Task task = new Task(0, null, LocalDateTime.now(), true);
-        repository.add(task);
+        String value = String.valueOf(System.currentTimeMillis());
+        User user = new User(0, value, value, value);
+        userRepository.add(user);
+        Task task = new Task(0, null, LocalDateTime.now(), true, user);
+        taskRepository.add(task);
         boolean done = task.isDone();
-        repository.setDoneById(task.getId(), false);
-        Task taskInDb = repository.findById(task.getId())
-                .orElse(new Task(0, null, LocalDateTime.now(), false));
+        taskRepository.setDoneById(task.getId(), false);
+        Task taskInDb = taskRepository.findById(task.getId())
+                .orElse(new Task(0, null, LocalDateTime.now(), false, null));
         assertThat(task).isEqualTo(taskInDb);
         assertThat(done).isNotEqualTo(taskInDb.isDone());
     }
